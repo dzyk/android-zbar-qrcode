f44a9ef1ee67a1d8ffe5fcd7205faffe217959c3
notebook <dzyk@mail.ru>
2017-01-29 21:21:48 +0300

    pzx

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''
 Qrcode example application
 ==========================
@@ -13,6 +15,30 @@ Featuring:
   detect Qrcode.
 
 '''
+import kivy
+kivy.require('1.9.1')
+
+from kivy.network.urlrequest import UrlRequest
+from kivy.properties import StringProperty, Clock, NumericProperty,\
+    DictProperty, BooleanProperty
+from kivy.uix.button import Button
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy import metrics
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from json import dumps
+import base64
+# my import
+from kivy.clock import Clock
+from kivy.adapters.listadapter import ListAdapter
+from kivy.uix.listview import ListItemButton, ListView
+import time
+import re
+
+
 
 
 __version__ = '1.0'
@@ -42,6 +68,14 @@ ImageFormat = autoclass('android.graphics.ImageFormat')
 LinearLayout = autoclass('android.widget.LinearLayout')
 Symbol = autoclass('net.sourceforge.zbar.Symbol')
 
+# my code insert
+#
+#
+#
+#
+
+
+
 
 class PreviewCallback(PythonJavaClass):
     '''Interface used to get back the preview frame of the Android Camera
@@ -67,7 +101,7 @@ class SurfaceHolderCallback(PythonJavaClass):
     def __init__(self, callback):
         super(SurfaceHolderCallback, self).__init__()
         self.callback = callback
- 
+
     @java_method('(Landroid/view/SurfaceHolder;III)V')
     def surfaceChanged(self, surface, fmt, width, height):
         self.callback(fmt, width, height)
@@ -75,7 +109,7 @@ class SurfaceHolderCallback(PythonJavaClass):
     @java_method('(Landroid/view/SurfaceHolder;)V')
     def surfaceCreated(self, surface):
         pass
- 
+
     @java_method('(Landroid/view/SurfaceHolder;)V')
     def surfaceDestroyed(self, surface):
         pass
@@ -219,6 +253,152 @@ class AndroidCamera(Widget):
 
 
 class ZbarQrcodeDetector(AnchorLayout):
+    # my code
+    #
+    #
+    #
+
+    # Auth properties
+    login = StringProperty(u'')
+    password = StringProperty(u'')
+    host = StringProperty('')
+
+    # Represents the last command which was accepted
+    last_accepted_command = StringProperty('')
+
+    # Bound to the "Info" label
+    info_text = StringProperty('')
+
+    # Command execution confirmation flag
+    need_confirm = BooleanProperty(True)
+
+# my functions
+#
+#
+#
+# data for friends list
+
+# --------------------
+    labell = Label(id = 'text_label',text='labell', halign='center',
+                             size_hint_y=None, width=metrics.dp(35))
+    friendlabel = Label(id = 'friend_label',text=' click >>', halign='center',
+                             size_hint_y=None, width=metrics.dp(35))
+    scorelabel = TextInput(id = 'score_label',text='0.99', halign='center',
+                             size_hint_y=None, width=metrics.dp(35), multiline=False)
+
+
+    textinput_addfriend = TextInput(id = 'txtaddfriend',text='', multiline=False)
+
+
+    def _send_request_dzyk(self, url, success=None, error=None, params=None):
+        headers = {
+            'User-Agent': 'Mozilla/5.0',
+            'Content-type': 'application/json',
+            'Authorization': self._get_auth()
+        }
+
+
+        UrlRequest(
+            url= self.host[:-14]+'com/' + url, timeout=30, req_headers=headers,
+            req_body=None if params is None else dumps(params),
+            on_success=success, on_error=error, on_failure=error)
+
+    def _get_commands_error_dzyk(self, request, error):
+        print  error
+
+
+
+    def _get_commands_result_dzyk_balance_allsum(self, request, response):
+        # Parses API-call response
+        try:
+            for i in  response:
+                self.labell.text =i['sum']
+        except Exception as e:
+            self._get_commands_error_dzyk(request, str(e))
+
+    def _get_commands_result_dzyk_friend(self, request, response):
+        try:
+            self._pnl_friend.clear_widgets()
+            c = 0
+            for i in  response:
+                if c < 10:
+                    st ="%s:%s:%s"%( i['friend'],i['friend_username'],i['btcaddress_friend'])
+                    self.data2[c]['text'] = st
+                    c += 1
+
+            self._pnl_friend.add_widget(self.list_view2)
+            self.list_adapter2.data.prop.dispatch(self.list_adapter2.data.obj())
+
+        except Exception as e:
+            self._get_commands_error_dzyk(request, str(e))
+
+    def gofrequest(self, request, response):
+        try:
+            self.friendlabel.text = response
+
+        except Exception as e:
+            self._get_commands_error_dzyk(request, str(e))
+
+
+
+    def refresh_friends(self, instance=None):
+        # Implements API-call "Get available command list"
+        self._send_request_dzyk(
+            'friend_get/', params=None,
+            success=self._get_commands_result_dzyk_friend, error=self._get_commands_error_dzyk)
+
+    def getonefriend(self, instance=None):
+        # Implements API-call "Get available command list"
+        self._send_request_dzyk(
+            'getonefriend/', params=None,
+            success=self.gofrequest, error=self._get_commands_error_dzyk)
+
+    def getonefriendback(self, instance=None):
+        # Implements API-call "Get available command list"
+        self._send_request_dzyk(
+            'getonefriendback/', params=None,
+            success=self.gofrequest, error=self._get_commands_error_dzyk)
+
+# add friend
+    def callback_request(self, request, response):
+        try:
+            print request
+        except Exception as e:
+            self._get_commands_error_dzyk(request, str(e))
+
+
+    def _dzyk_add_friend(self, instance=None):
+        p = {'friend_btc':'this field must be a modified', 'comment':'my project'}
+        px = ''
+
+        px=str(self.textinput_addfriend.text)
+        try:
+            px = re.search(r'^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$',px).group(0)
+        except Exception as e:
+            px = '1PRpSMEd9TkjRodApnFBt2uEhGEDPVutyy'
+            print e
+
+        p['friend_btc']=px
+        #^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$ p['comment']=re.search(r'^[0-9]+',sendname).group(0)
+        self._send_request_dzyk(
+            'add_friend/', params=p,
+            success=self.callback_request, error=self._get_commands_error_dzyk)
+
+        time.sleep(2)
+
+# send score function
+    def _dzyk_send(self, instance=None):
+        p = {'friend':'1', 'score':'0.77'}
+        p['score']=self.scorelabel.text
+        p['friend']=self.friendlabel.text
+        self._send_request_dzyk(
+            'score_change/', params=p,
+            success=self.callback_request, error=self._get_commands_error_dzyk)
+#
+
+
+
+
     '''Widget that use the AndroidCamera and zbar to detect qrcode.
     When found, the `symbols` will be updated
     '''
@@ -234,6 +414,25 @@ class ZbarQrcodeDetector(AnchorLayout):
 
     def __init__(self, **kwargs):
         super(ZbarQrcodeDetector, self).__init__(**kwargs)
+        # my code
+        #
+        #
+
+        # ID of the command being executed
+        self._cmd_id = None
+
+        # List of the "completed" statuses.
+        # The first status should be "Done"
+        self._completed = []
+
+        # If True - sends a request to retrieve a command status
+        self._wait_completion = False
+
+        # requested command
+        self._command = (0, '')
+
+
+        #----------------------------------
         self._camera = AndroidCamera(
                 size=self.camera_size,
                 size_hint=(None, None))
